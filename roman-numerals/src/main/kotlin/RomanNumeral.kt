dc9d9c07671887c1885fc12aa17c12e485e263ee
ricardoliveira0 <l42647@alunos.uevora.pt>
2019-11-04 18:29:17 +0000

    Exercise- Roman Numerals

--- roman-numerals/src/main/kotlin/RomanNumeral.kt
+++ roman-numerals/src/main/kotlin/RomanNumeral.kt
@@ -1,7 +1,41 @@
+class RomanNumeral {
+    companion object {
+        fun value(inp: Int): String {
+            val stringBuilder = StringBuilder()
 
-in (6..8) -> five + (6..base).map(){one}.joinToString("")
-5 -> "" + five
-4 -> "" + one + five
-in (1..3) -> (1..base).map(){one}.joinToString("")
-0 -> ""
-else -> throw IllegalStateException("Math is broken ${base}")
+            val thousands = inp / 1000
+            stringBuilder.append(times(thousands, "M"))
+
+            val hundreds = inp / 100 % 10
+            stringBuilder.append(times(hundreds, "C", "D", "M"))
+
+            val tens = inp / 10 % 10
+            stringBuilder.append(times(tens, "X", "L", "C"))
+
+            val ones = inp % 10
+            stringBuilder.append(times(ones, "I", "V", "X"))
+
+
+            return String(stringBuilder)
+        }
+
+        private fun times(number: Int, char: String): String {
+            val stringBuilder = StringBuilder()
+            for (i in 0.. number - 1) {
+                stringBuilder.append(char)
+            }
+            return String(stringBuilder)
+        }
+
+        private fun times(number: Int, onesChar: String, fivesChar: String, tensChar: String): String {
+            when (number) {
+                0 -> return ""
+                1, 2, 3 -> return times(number, onesChar)
+                4 -> return onesChar + fivesChar
+                5, 6, 7, 8 -> return fivesChar + times(number - 5, onesChar)
+                9 -> return onesChar + tensChar
+                else -> throw IllegalArgumentException("Only single digits allowed - not " + number)
+            }
+        }
+    }
+}
