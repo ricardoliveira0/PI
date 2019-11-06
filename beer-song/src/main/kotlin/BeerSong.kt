393e3b11ba68fa436f32a99fd4287d72ce36da25
ricardoliveira0 <l42647@alunos.uevora.pt>
2019-11-06 22:55:30 +0000

    Exercise - BeerSong

--- beer-song/src/main/kotlin/BeerSong.kt
+++ beer-song/src/main/kotlin/BeerSong.kt
@@ -8,11 +8,13 @@ object BeerSong {
         val singularOrPlural = if (n - 1 > 1) "bottles" else "bottle"
 
         return when (n) {
-            in 2..99 -> "$n bottles of beer on the wall, $n bottles of beer.\nTake one down and pass it around, ${n-1} $bottleOrBottles of beer on the wall.\n"
+            in 2..99 -> "$n bottles of beer on the wall, $n bottles of beer.\nTake one down and pass it around, ${n-1} $singularOrPlural of beer on the wall.\n"
             1 -> "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
             0 -> "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
             else -> ""
         }
-        
     }
+
+    fun songVerses(startingBottles: Int, endingBottles: Int): String =
+        (startingBottles downTo endingBottles).map { songVerses(it) }.joinToString(separator = "\n")
 }
