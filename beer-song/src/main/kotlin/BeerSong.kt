211c34c19e4a3b79b450a8ef1895b0657ef59345
ricardoliveira0 <l42647@alunos.uevora.pt>
2019-11-06 22:38:52 +0000

    Update Exercise - BeerSong

--- beer-song/src/main/kotlin/BeerSong.kt
+++ beer-song/src/main/kotlin/BeerSong.kt
@@ -5,6 +5,14 @@ object BeerSong {
     fun songVerses(n: Int): String {
         require(n in (0..99)) { "Beer song can only start at 99 and end at 0!" }
 
-        val bottleOrBottles = if (n - 1 > 1) "bottles" else "bottle"
+        val singularOrPlural = if (n - 1 > 1) "bottles" else "bottle"
+
+        return when (n) {
+            in 2..99 -> "$n bottles of beer on the wall, $n bottles of beer.\nTake one down and pass it around, ${n-1} $bottleOrBottles of beer on the wall.\n"
+            1 -> "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+            0 -> "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+            else -> ""
+        }
+        
     }
 }
