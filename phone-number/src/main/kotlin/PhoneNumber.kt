4b6361c52a5f73a39c80295d118a438c3db5b376
ricardoliveira0 <l42647@alunos.uevora.pt>
2019-11-07 00:56:21 +0000

    Update Exercise - PhoneNumber

--- phone-number/src/main/kotlin/PhoneNumber.kt
+++ phone-number/src/main/kotlin/PhoneNumber.kt
@@ -5,4 +5,18 @@ data class PhoneNumber(val input: String) {
     private val middleNo = number.drop(3).take(3)
     private val lastNo = number.takeLast(4)
 
+    private fun isTenDigit(): String { 
+    	val digits = input.filter { it.isDigit() } * Caso tenha o primeiro digito (1), este será removido.
+
+    	if (digits.first() == '1') {
+        	require(digits.length == 11) { "11 digits with country code (1)" }
+        	return digits.drop(1)
+    	}
+
+    	require(digits.length == 10) { "Must be 10 digits excluding country code" }
+    	return digits
+    }
+
+
+
 }
