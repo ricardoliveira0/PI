bbab08e926e65eb82a505335b1fe11aabab9a970
Anastac1o <duarte_anastacio15@hotmail.com>
2019-11-28 14:31:11 +0000

    Updating diamond

--- src/diamond/src/main/kotlin/Diamond.kt
+++ src/diamond/src/main/kotlin/Diamond.kt
@@ -1,5 +1,15 @@
 class DiamondPrinter {
-
-
-
+    fun printToList(given: Char): List<String> {
+        val range = ('A'..given).toList()
+        val size = range.size * 2 - 1
+        val middle = size / 2
+        val result = mutableListOf<String>()
+        range.forEachIndexed { i, c ->
+            val line = CharArray(size) { ' ' }
+            line[middle - i] = c
+            line[middle + i] = c
+            result.add(line.joinToString(""))
+        }
+        return result + result.dropLast(1).reversed()
+    }
 }
