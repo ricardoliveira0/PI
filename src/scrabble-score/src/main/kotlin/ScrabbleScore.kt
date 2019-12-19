4f5e412d1fe49b6e31311e00512e0cd14d1b3bf0
ricardoliveira0 <l42647@alunos.uevora.pt>
2019-12-19 01:12:32 +0000

    Update Exercise - ScrabbleScore

--- src/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ src/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -4,7 +4,17 @@ object ScrabbleScore {
         TODO("Implement this function to complete the task")
     }
 
-    fun scoreWord(word: String): Int {
-        TODO("Implement this function to complete the task")
-    }
+    fun scoreWord(word: String): Int =
+        word.toUpperCase().sumBy {
+            when (it) {
+                'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' -> 1
+                'D', 'G' -> 2
+                'B', 'C', 'M', 'P' -> 3
+                'F', 'H', 'V', 'W', 'Y' -> 4
+                'K' -> 5
+                'J', 'X' -> 8
+                'Q', 'Z' -> 10
+                else -> throw IllegalArgumentException()
+            }
+        }
 }
