3f51102a7672d8b8ddedbabb0cb3684899960a14
ricardoliveira0 <l42647@alunos.uevora.pt>
2019-12-19 01:25:34 +0000

    Finish Exercise - ScrabbleScore

--- src/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ src/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,9 +1,5 @@
 object ScrabbleScore {
 
-    fun scoreLetter(c: Char): Int {
-        TODO("Implement this function to complete the task")
-    }
-
     fun scoreWord(word: String): Int =
         word.toUpperCase().sumBy {
             when (it) {
